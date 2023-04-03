96e10f68b1bc86371fd35a9b66bbf1eea1c0efed
Seyfullah sahin <seyfullahsahinozdemir@gmail.com>
2023-04-03 22:04:11 +0300

    Product Listing and Pagination

--- src/app/services/common/models/product.service.ts
+++ src/app/services/common/models/product.service.ts
@@ -1,19 +1,42 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Create_Product } from 'src/app/contracts/create_product';
+import { List_Product } from 'src/app/contracts/list_product';
 import { HttpClientService } from '../http-client.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  constructor(private httpClientService: HttpClientService) {}
 
-  constructor(private httpClientService: HttpClientService) { }
-
-  create(product: Create_Product, successCallBack?: any) {
+  create(product: Create_Product, successCallBack?: () => void, errorCallBack?: (errorMessage: string) => void) {
     this.httpClientService.post({
       controller: "products"
-    },product).subscribe(result => {
-      successCallBack();
-    })
+    }, product)
+      .subscribe(result => {
+        successCallBack();
+      }, (errorResponse: HttpErrorResponse) => {
+        let message = "";
+        let _error: {key: string, values: Array<string>}[] = errorResponse.error;
+
+        for (let key in _error) {
+          let value = _error[key];   
+          message += `${value}<br>`       
+        }
+        errorCallBack(message);
+      });
+  }
+
+  async read(page: number = 0, size: number = 5, successCallBack?: () => void, errorCallBack?: (errorMessage: string) => void): Promise<{totalCount: number, products: List_Product[]}> {
+    const promiseData : Promise<{totalCount: number, products: List_Product[]}> = this.httpClientService.get<{totalCount: number, products: List_Product[]}>({
+      controller: "products",
+      queryString: `page=${page}&size=${size}`
+    }).toPromise();
+
+    promiseData.then(d => successCallBack())
+    .catch((errorResponse: HttpErrorResponse) => errorCallBack(errorResponse.message))
+
+    return await promiseData;
   }
 }
